594b627969d1e72f33300b1ffba94480a388bfd1
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-08-28 17:38:05 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+function add(x,y){
+    return(x + y)
+}
+
+add(2,4)
+
+function subtract(x,y){
+    return(x - y)
+}
+
+subtract(8,4)
+
+function multiply(x,y){
+    return(x * y)
+}
+
+multiply(2,4)
+
+function divide(x,y){
+    return(x / y)
+}
+
+divide(4,2)
+
+function increment(n){
+    return(++n)
+}
+
+increment(4)
+
+function decrement(n){
+    return(--n)
+}
+
+decrement(4)
+
+function makeInt(n)
+{
+    return(parseInt(n,10))
+}
+
+makeInt(0)
+
+function preserveDecimal(n)
+{
+    return (parseFloat(n))
+}
+
+preserveDecimal(3.0123)
